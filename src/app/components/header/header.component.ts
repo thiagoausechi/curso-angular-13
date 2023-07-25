4918cb45f50eef226aed1976a544beea9068706c
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-07-25 11:27:43 -0300

    feat: :sparkles: Add Header Nav using Component Properties

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+type Link = {
+  display: string;
+  link: string;
+};
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  links: Link[] = [
+    { display: 'Home', link: '/' },
+    { display: 'About', link: '/about' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
