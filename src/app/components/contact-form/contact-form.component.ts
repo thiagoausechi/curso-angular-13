b4e91185434e627980db16443a3a6fccb2b1ac05
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-07-26 10:40:58 -0300

    feat: :sparkles: Add Two-Way Data Binding to Contect Form

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
+  styleUrls: ['./contact-form.component.css'],
 })
 export class ContactFormComponent implements OnInit {
+  name: string = '';
+  email: string = '';
+  content: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
