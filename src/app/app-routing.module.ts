2d7f5e8245a4b40e65b943da1ad9cee7cd898e6b
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-07-26 11:18:47 -0300

    fix: :adhesive_bandage: Fix components in routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 interface NamedRoute extends Route {
   name: string;
@@ -8,8 +10,8 @@ interface NamedRoute extends Route {
 }
 
 export const routes: NamedRoute[] = [
-  { path: '',      name: 'Home',  component: AppComponent },
-  { path: 'about', name: 'About', component: AppComponent },
+  { path: '',      name: 'Home',  component: HomeComponent  },
+  { path: 'about', name: 'About', component: AboutComponent },
 ];
 
 @NgModule({
