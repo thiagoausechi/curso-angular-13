07242c8a67ac3b226288b37419dacf2efa298f48
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-07-27 10:53:11 -0300

    feat: :lipstick: Update Todo Component style

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+type Task = {
+  name: string;
+  done: boolean;
+};
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
+  tasks: Task[] = [
+    {
+      name: 'Test',
+      done: false,
+    },
+    {
+      name: 'Test 2',
+      done: true,
+    },
+    {
+      name: 'Really long task name because I can, because I will and because I need',
+      done: false,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
