b02ec28f0e131f43d6a979dae32351c81e707488
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-07-27 08:52:32 -0300

    feat: :lipstick: Update User page

--- src/app/pages/user-page/user-page.component.ts
+++ src/app/pages/user-page/user-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
-  styleUrls: ['./user-page.component.css']
+  styleUrls: ['./user-page.component.css'],
 })
 export class UserPageComponent implements OnInit {
+  user: User = null;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const userID = this.route.snapshot.paramMap.get('id') || '0';
+    this.userService
+      .getUserByID(userID)
+      .subscribe((data) => (data && 'id' in data ? (this.user = data) : null));
   }
-
 }
